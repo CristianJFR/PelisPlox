e9d0c8f7398bc543c7b91006aba5c437ee04893d
Cristian Fernandez <FernandezRuizCristiann@gmail.com>
2022-07-11 09:58:30 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>PelisPlox</title>
+
+  <!-- Latest compiled and minified CSS -->
+  <link rel="Stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <link rel="shortcut icon" href="upload/logo.png" type="image/x-icon">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+
+<body class="Home">
+
+  <h1>Aca va la pagina de inicio</h1>
+
+  <!-- Bootstrap core JavaScript
+    ================================================== -->
+  <!-- Placed at the end of the document so the pages load faster -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="https://kit.fontawesome.com/8c2a2ac076.js" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
